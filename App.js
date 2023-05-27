d6bae4e0ae17e637bc36b3f9124518c9e1457ddc
Latifa <latifa@Latifas-MacBook-Pro-2.local>
2023-05-27 09:41:14 -0400

    .

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
-
+ 
     </TailwindProvider>
     </NavigationContainer>
   );
