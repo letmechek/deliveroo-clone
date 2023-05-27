baa632db2a5d2562ecc746efbc02602220a96c9b
Latifa <latifa@Latifas-MacBook-Pro-2.local>
2023-05-27 11:34:18 -0400

    adds homescreen image and icons

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 
 
---
+++ screens/HomeScreen.js
@@ -0,0 +1,62 @@
+import { Text, View, SafeAreaView, Image, ScrollView} from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsHorizontalIcon } from 'react-native-heroicons/outline'
+import { TextInput } from 'react-native'
+
+const HomeScreen = () => {
+    const navigation = useNavigation()
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, [])
+   
+    return (
+      <SafeAreaView className='bg-white pt-5'>
+        <Text className='text-red-500'> 
+        {/* Header */}
+            <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+                <Image source={{
+                    uri: "https://links.papareact.com/wru",
+                }}
+                    className="h-7 w-7 bg-gray-300 rounded-full "
+                />
+                <View className='flex-1'>
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+                    <Text className="font-bold text-xl">
+                        Current Location
+                        <ChevronDownIcon size={20} color='#00CCBB' />
+                    </Text>
+                </View>
+                <UserIcon size={35} color='#00CCBB' />
+            </View>
+                {/* Search */}
+                <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+                    <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                    <MagnifyingGlassIcon color='gray' size={20}/>
+                    <TextInput placeholder='resteraunts and cuisines'
+                    keyboardType='default'/>
+                    </View>
+                <AdjustmentsHorizontalIcon color='#00CCBB'/>
+                </View>
+               {/* Body */}
+               <ScrollView>
+                {/* categories */}
+
+                {/* featured rows */}
+
+               </ScrollView>
+
+        </Text>
+        </SafeAreaView>
+
+
+
+    )
+  
+}
+
+export default HomeScreen
+
